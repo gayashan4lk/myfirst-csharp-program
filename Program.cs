b63e1df83d7860285496f279682597f2e0914626
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-03-20 22:55:24 +0530

    finish program

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ namespace MyFirstProgram
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Where should I go in May?");
+            WeatherUtilities.Report("San Francisco", WeatherUtilities.FahrenheitToCelsius(65), 73);
+            WeatherUtilities.Report("Denver", WeatherUtilities.FahrenheitToCelsius(77), 55);
             WeatherUtilities.Report("Colombo", 30, 85);
         }
     }
