ae0b806149fde5d12283de6610e6d2bf5a5a2176
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-03-20 10:22:38 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+}
