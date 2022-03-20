37c449b00c1e7560bdb61ad8f4211ca88f0092aa
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-03-20 10:24:55 +0530

    add WeatherUtilities class

---
+++ WeatherUtilities.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyUtilities
+{
+    class WeatherUtilities
+    {
+        static float FahrenheitToCelsius(float tempFahrenheit)
+        {
+            var tempCelsius = tempFahrenheit - 32 / 1.8f;
+            return tempCelsius;
+        }
+
+        static float CelciusToFahrenheit(float tempCelcius)
+        {
+            var tempFahrenheit = tempCelcius * 1.8f + 32;
+            return tempFahrenheit;
+        }
+
+        static float ComfortIndex(float tempFahrenheit, float humidityPercent)
+        {
+            var comfortIndex = (tempFahrenheit + humidityPercent) / 4;
+            return comfortIndex;
+        }
+
+        static void Report(string location, float tempCelsius, float humidity)
+        {
+            var tempFahrenheit = CelciusToFahrenheit(tempCelsius);
+            Console.WriteLine("Confort Index for " + location + " : " + ComfortIndex(tempFahrenheit, humidity));
+        }
+    }
+}
